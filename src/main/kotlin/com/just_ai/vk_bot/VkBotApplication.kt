78f141c51eb8ca732c554f1ffd3f98e444b9a0b1
Test User <jabrimuhi@test.com>
2024-07-16 14:21:06 +0300

    feat: initial vk bot commit

---
+++ src/main/kotlin/com/just_ai/vk_bot/VkBotApplication.kt
@@ -0,0 +1,11 @@
+package com.just_ai.vk_bot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VkBotApplication
+
+fun main(args: Array<String>) {
+    runApplication<VkBotApplication>(*args)
+}
---
+++ src/test/kotlin/com/just_ai/vk_bot/VkBotApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.just_ai.vk_bot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VkBotApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
